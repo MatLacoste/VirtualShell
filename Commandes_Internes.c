6d01ffb03238c37d803f38fd9e94089dac2cad3f
MatLacoste <mathieu33_lacoste@hotmail.fr>
2016-01-08 10:38:56 +0100

    Début implémentation des commandes internes. Commandes implémentés : pwd, cd, hostname

--- Commandes_Internes.c
+++ Commandes_Internes.c
@@ -1,4 +1,24 @@
 #include "Commandes_Internes.h"
 
+char* myPwd () {
+	char cwd[1024];
+	getcwd (cwd, sizeof(cwd));
+	return cwd;
+}
 
+void myCd (char* name) {
+	char* pwd = myPwd ();
+	strcat (pwd, "/");
+	strcat (pwd, name);
+	if (chdir (pwd) == -1)
+		printf ("Le dossier %s n'existe pas\n", name);
+	else
+		printf ("Vous êtes entré dans le dossier %s\n", name);
+}
+
+char* myHostname () {
+	char hostname[1024];
+	gethostname (hostname, sizeof (hostname));
+	return hostname;
+}
 
--- Commandes_Internes.h
+++ Commandes_Internes.h
@@ -1,6 +1,7 @@
 #ifndef COMINTERN_H
 #define COMINTERN_H
 #include "Shell.h"
+#include <unistd.h>
 
 
 
