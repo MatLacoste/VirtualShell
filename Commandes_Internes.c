1b4b618ed93ce17a368d387acb3de0ebb0d53722
MatLacoste <mathieu33_lacoste@hotmail.fr>
2016-01-11 09:23:09 +0100

    Ajout du History, Echo et Date

--- Commandes_Internes.c
+++ Commandes_Internes.c
@@ -1,5 +1,46 @@
 #include "Commandes_Internes.h"
 
+void myEcho (char *sentence) {
+	printf ("%s\n", sentence);
+}
+
+void myDate () {
+	time_t t = time(NULL);
+	struct tm tm = *localtime(&t);
+	int hours = tm.tm_gmtoff/3600;
+	int min = tm.tm_gmtoff - (hours*3600);
+	char positif;
+
+	if (hours > 0) { 
+		positif = '+';
+	} else {
+		positif = '-';
+		hours *= -1;
+		min *= -1;
+	}
+
+	printf("%s %d %s %d, %02d:%02d:%02d (UTC%c%02d%02d)\n", JOURS[tm.tm_wday], tm.tm_mday, MOIS[tm.tm_mon], tm.tm_year + 1900, tm.tm_hour, tm.tm_min, tm.tm_sec, positif, hours, min);	
+}
+
+void myHistory () {
+	int i;
+	for (i=0; i<NB_COMMAND; i++)
+			printf ("%d  %s\n", (i+1), COMMAND_LIST[i]);
+}
+
+void addCommand (char* newCommand) {
+	int i;
+	if (NB_COMMAND == NB_COMMAND_MAX) {
+		for (i=0; i<NB_COMMAND_MAX-1; i++) {
+			 strcpy(COMMAND_LIST[i], COMMAND_LIST[i+1]);
+		}
+		strcpy(COMMAND_LIST[NB_COMMAND_MAX-1], newCommand);
+	} else {
+		strcpy(COMMAND_LIST[NB_COMMAND], newCommand);
+		NB_COMMAND++;
+	}
+}
+
 char* myPwd () {
 	char cwd[1024];
 	getcwd (cwd, sizeof(cwd));
--- Commandes_Internes.h
+++ Commandes_Internes.h
@@ -2,7 +2,20 @@
 #define COMINTERN_H
 #include "Shell.h"
 #include <unistd.h>
+#include <string.h>
 
+#define NB_COMMAND_MAX 50
 
+char COMMAND_LIST[NB_COMMAND_MAX][1024];
+int NB_COMMAND=0;
+char JOURS[7][10] = {"dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi"};
+char MOIS[12][10] = {"janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "novembre", "decembre"};
+
+void myEcho (char *sentence);
+void myDate ();
+void myHistory ();
+void addCommand (char* newCommand);
+char* myPwd ();
+void myCd (char* name);
 
 #endif
