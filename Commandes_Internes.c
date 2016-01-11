91eb0bb1aab5587f1af8eef36e816c069e2ede51
MatLacoste <mathieu33_lacoste@hotmail.fr>
2016-01-11 11:23:00 +0100

    correction

--- Commandes_Internes.c
+++ Commandes_Internes.c
@@ -5,6 +5,8 @@ void myEcho (char *sentence) {
 }
 
 void myDate () {
+	char JOURS[7][10] = {"dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi"};
+	char MOIS[12][10] = {"janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "novembre", "decembre"};
 	time_t t = time(NULL);
 	time_t gmt = 0;
 	time_t offset = mktime(gmtime(&gmt));
@@ -25,22 +27,32 @@ void myDate () {
 	printf("%s %d %s %d, %02d:%02d:%02d (UTC%c%02d%02d)\n", JOURS[tm.tm_wday], tm.tm_mday, MOIS[tm.tm_mon], tm.tm_year + 1900, tm.tm_hour, tm.tm_min, tm.tm_sec, positif, hours, min);	
 }
 
+int nb_command () {
+	int i=0;
+	int cpt =0;
+	for (i =0; i < NB_COMMAND_MAX; i++)
+		if(COMMAND_LIST[i] != NULL)
+			cpt++;
+	return cpt;
+}
+
 void myHistory () {
 	int i;
-	for (i=0; i<NB_COMMAND; i++)
+	int nb = nb_command ();
+
+	for (i=0; i<nb; i++)
 			printf ("%d  %s\n", (i+1), COMMAND_LIST[i]);
 }
 
 void addCommand (char* newCommand) {
 	int i;
-	if (NB_COMMAND == NB_COMMAND_MAX) {
+	if (nb_command () == NB_COMMAND_MAX) {
 		for (i=0; i<NB_COMMAND_MAX-1; i++) {
 			 strcpy(COMMAND_LIST[i], COMMAND_LIST[i+1]);
 		}
 		strcpy(COMMAND_LIST[NB_COMMAND_MAX-1], newCommand);
 	} else {
-		strcpy(COMMAND_LIST[NB_COMMAND], newCommand);
-		NB_COMMAND++;
+		strcpy(COMMAND_LIST[nb_command()], newCommand);
 	}
 }
 
--- Commandes_Internes.h
+++ Commandes_Internes.h
@@ -8,15 +8,13 @@
 #define NB_COMMAND_MAX 50
 
 char COMMAND_LIST[NB_COMMAND_MAX][1024];
-int NB_COMMAND=0;
-char JOURS[7][10] = {"dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi"};
-char MOIS[12][10] = {"janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "novembre", "decembre"};
 
-void myEcho (char *sentence);
+void myEcho (char *);
 void myDate ();
 void myHistory ();
-void addCommand (char* newCommand);
+void addCommand (char *);
 char* myPwd ();
-void myCd (char* name);
+void myCd (char *);
+char* myHostname ();
 
 #endif
